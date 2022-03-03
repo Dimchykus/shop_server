8b89d1998b4f255fcae3cb45346272d0ac648963
Dimchykus <62378746+Dimchykus@users.noreply.github.com>
2022-03-03 14:50:49 +0200

    add one model, connected to mongodb

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
-import  http from "http";
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var ItemModel = require("./models/item");
 
-const PORT = process.env.PORT || 4000
+const conStr =
+  "mongodb+srv://Dima:Dima123123@cluster0.hmfva.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose
+  .connect(conStr, (err)=>{
+    if(err) throw err;
+    console.log('Connected')
+  });
 
-var app = http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  var message = "It works!\n",
-    version = "NodeJS " + process.versions.node + "\n",
-    response = [message, version].join("\n");
-  res.end(response);
+var app = express();
+app.use(express.json());
+
+var router = express.Router();
+
+router.get("/", (req, res, next) => {
+  ItemModel.find({}, function (err, items) {
+    if (err) res.send("error");
+    res.send(items);
+  });
 });
 
+const PORT = process.env.PORT || 4000;
+
+var awesome_instance = new ItemModel({ title: "awesome" });
+
+// Save the new model instance, passing a callback
+awesome_instance.save(function (err) {
+  if (err) return console.log(err);
+  console.log("saved");
+});
+
+app.use(router);
+
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  console.log(`Listening ${PORT}`);
 });
